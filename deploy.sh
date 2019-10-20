60a05dd4d06a49d5f0ecb61043ad1b3d3012a062
Jaz Singh <jazeee@gmail.com>
2019-10-20 16:23:04 -0700

    Handle outdated readings correctly. Update deploy notes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 
 cd android
 ./gradlew assembleRelease
-/opt/android-sdk/build-tools/28.0.3/apksigner sign --ks ~/.gradle/jazcomkeystore --out app/build/outputs/apk/release/app-release.apk app/build/outputs/apk/release/app-release-unsigned.apk
+echo "Michael Jackson"
+/opt/android-sdk/build-tools/28.0.3/apksigner sign \
+  --ks ~/.gradle/jazcomkeystore \
+  --ks-key-alias jazcomkeystore \
+  --out app/build/outputs/apk/release/app-release.apk \
+  app/build/outputs/apk/release/app-release-unsigned.apk
 
-adb install -rf app/build/outputs/apk/release/app-release.apk
+adb install -f app/build/outputs/apk/release/app-release.apk
