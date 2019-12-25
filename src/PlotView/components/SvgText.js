3e1735962a75ceee1ae1b2a8b3db2ba5df0a36d5
Jaz Singh <jsingh@gmail.com>
2019-12-24 17:17:31 -0800

    Reorganize code, upgrade packages

---
+++ src/PlotView/components/SvgText.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native-svg';
+
+type Props = {
+  x: number,
+  y: number,
+  yOffset?: number,
+  color: string,
+  children: string,
+};
+
+const SvgText = (props: Props) => {
+  const { x, y, yOffset = 4, children, color, ...otherProps } = props;
+  return (
+    <Text
+      fill={color}
+      stroke="none"
+      fontSize="12"
+      x={x}
+      y={y - yOffset}
+      textAnchor="start"
+      opacity={0.5}
+      {...otherProps}
+    >
+      {children}
+    </Text>
+  )
+}
+
+export default SvgText;
