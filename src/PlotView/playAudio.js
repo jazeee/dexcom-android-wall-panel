c1572b6b96eb99b3cff69d1330e4fee3ad8cf60a
Jaz Singh <jsingh@gmail.com>
2019-12-26 14:49:03 -0800

    Add audible alarm for low value

---
+++ src/PlotView/playAudio.js
@@ -0,0 +1,19 @@
+import Sound from 'react-native-sound';
+
+export const playAudio = (audioName: string) => {
+  return new Promise((resolve, reject) => {
+    const sound = new Sound(audioName, null, error => {
+      if (error) {
+        return reject(error);
+      }
+      sound.play(resolve);
+    });
+  });
+};
+
+export const safePlayAudio = (audioName: string) => {
+  console.log(`Playing ${audioName}`);
+  playAudio(audioName)
+    .then(result => console.log(`Successfully played ${audioName}`, result))
+    .catch(error => console.error(`Failed to play ${audioName}`, error));
+};
