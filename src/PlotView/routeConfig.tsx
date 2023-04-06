a3af903a7a6637fa188105ea0fa033cecad00d83
Jaz Singh <jazeee@gmail.com>
2023-04-05 17:42:36 -0700

    Add Time back to header

--- src/PlotView/routeConfig.tsx
+++ src/PlotView/routeConfig.tsx
@@ -1,11 +1,13 @@
 import { WrappedPlotView } from './PlotView';
 
 import { PlotViewHeaderButtons } from './Header';
+import { DateTime } from '../common/components/DateTime';
 
 export const plotViewRouteConfig = {
   name: 'PlotView',
   component: WrappedPlotView,
   options: {
+    headerTitle: () => <DateTime />,
     headerRight: () => <PlotViewHeaderButtons />,
   },
 };
