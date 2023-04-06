9e47414b33016b9e6b1a30730cd08408fffa943c
Jaz Singh <jazeee@gmail.com>
2023-04-05 17:42:36 -0700

    Add Time back to header

--- src/Home/routeConfig.tsx
+++ src/Home/routeConfig.tsx
@@ -1,11 +1,13 @@
 import { Home } from './Home';
 
 import { HomeHeaderButtons } from './Header';
+import { DateTime } from '../common/components/DateTime';
 
 export const homeRouteConfig = {
   name: 'Home',
   component: Home,
   options: {
+    headerTitle: () => <DateTime />,
     headerRight: () => <HomeHeaderButtons />,
   },
 };
