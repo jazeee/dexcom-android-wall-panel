152214dee0b15a6ed0b2495c9e9fa2328e586d4d
Jaz Singh <jazeee@gmail.com>
2024-04-29 10:17:29 -0700

    Finalize refactor of `PlotView`
    * Clean up types
    * Remove `WrappedPlotView`

--- src/PlotView/routeConfig.tsx
+++ src/PlotView/routeConfig.tsx
@@ -1,11 +1,11 @@
-import { WrappedPlotView } from './PlotView';
+import { PlotView } from './PlotView';
 
 import { PlotViewHeaderButtons } from './Header';
 import { DateTime } from '../common/components/DateTime';
 
 export const plotViewRouteConfig = {
   name: 'PlotView',
-  component: WrappedPlotView,
+  component: PlotView,
   options: {
     headerTitle: () => <DateTime />,
     headerRight: () => <PlotViewHeaderButtons />,
