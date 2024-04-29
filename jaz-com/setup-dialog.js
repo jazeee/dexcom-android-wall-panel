8825b34ed897c2f90f9d86b6dfd05a2d727450d5
Jaz Singh <jazeee@gmail.com>
2024-04-29 10:17:27 -0700

    Working application

---
+++ jaz-com/setup-dialog.js
@@ -0,0 +1,96 @@
+/**
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Dialog from "react-native-dialog";
+
+type Props = {
+  username: string,
+  password: string,
+  setCreds: (string, string) => any,
+};
+
+type State = {
+  username: string,
+  password: string,
+  isSetupDialogVisible: boolean,
+}
+
+export class JazComAccountDialog extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: props.username || "",
+      password: props.password || "",
+      isSetupDialogVisible: false,
+    };
+  }
+
+  onAccept = () => {
+    this.setState({ isSetupDialogVisible: false });
+    const { username, password } = this.state;
+    this.props.setCreds(username, password);
+  }
+  render () {
+    const { username, password, isSetupDialogVisible } = this.state;
+    return (
+      <View>
+        <Button
+          onPress={() => this.setState({ isSetupDialogVisible: true })}
+          title={`Set up Account`}
+          color="#841584"
+          accessibilityLabel="Set up Account"
+        />
+        <Dialog.Container visible={isSetupDialogVisible}>
+          <Dialog.Title>Account</Dialog.Title>
+          <Dialog.Description>
+            Enter Credentials.
+          </Dialog.Description>
+          <Dialog.Input
+            placeholder="Username"
+            value={username}
+            autoCapitalize="none"
+            autoComplete="username"
+            autoCorrect={false}
+            autoFocus
+            onChangeText={(username) => this.setState({ username })}
+          />
+          <Dialog.Input
+            placeholder="Password"
+            value={password}
+            autoCapitalize="none"
+            autoComplete="password"
+            autoCorrect={false}
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+          />
+          <Dialog.Button
+            onPress={() => this.setState({ isSetupDialogVisible: false })}
+            label="Cancel"
+          />
+          <Dialog.Button
+            onPress={this.onAccept}
+            label="Accept"
+          />
+        </Dialog.Container>
+      </View>
+    )
+  }
+}
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#002',
+//   },
+//   value: {
+//     fontSize: 160,
+//     color: "#841584",
+//   },
+// });
---
+++ lights/light-switch.js
@@ -0,0 +1,69 @@
+/**
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+type Props = {
+  switchId: number,
+};
+
+type State = {
+  response: string,
+}
+
+export class LightSwitch extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: "test",
+    };
+  }
+
+  toggleSwitch = async (switchId) => {
+    try {
+      const postResult = await fetch(`http://10.2.1.${switchId}/toggle-relay`, {
+        method: "POST",
+      });
+      const { status } = postResult;
+      if (status !== 200) {
+        throw new Error("Unable to Post");
+      }
+      // const response = await postResult.text();
+      // this.setState({ response });
+    } catch (error) {
+      console.log(error);
+      this.setState({ response: error.toString() });
+    };
+  }
+  render() {
+    const { switchId } = this.props;
+    const { response } = this.state;
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.toggleSwitch.bind(this, switchId)}
+          title={`Toggle ${switchId}`}
+          color="#841584"
+          accessibilityLabel="Toggle Switch"
+        />
+        <Text style={styles.response}>{response}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+  },
+  response: {
+    color: "#f00",
+  },
+});
