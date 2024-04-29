c9f19a9311e7985639205a99dd7d8656f265dafc
Jaz Singh <jazeee@gmail.com>
2024-04-29 10:17:27 -0700

    Convert build to Android App Bundle

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-pushd android 
-./gradlew assembleRelease
+echo "See https://facebook.github.io/react-native/docs/signed-apk-android"
+
+pushd android
+# Assemble release would generate app-release.apk
+#./gradlew assembleRelease
+# Bundle release is recommended.
+./gradlew bundleRelease
 popd
 
-echo "Michael Jackson"
-/opt/android-sdk/build-tools/28.0.3/apksigner sign \
-  --ks ~/.gradle/jazcomkeystore \
-  --ks-key-alias jazcomkeystore \
-  --out android/app/build/outputs/apk/release/app-release-signed.apk \
-  android/app/build/outputs/apk/release/app-release.apk
+ls -la android/app/build/outputs/bundle/release/app.aab
+#
+# echo "Michael Jackson"
+# /opt/android-sdk/build-tools/28.0.3/apksigner sign \
+#   --ks ~/.gradle/jazcomkeystore \
+#   --ks-key-alias jazcomkeystore \
+#   --out android/app/build/outputs/apk/release/app-release-signed.apk \
+#   android/app/build/outputs/apk/release/app-release.apk
+
+echo "Run npx react-native run-android --variant=release"
